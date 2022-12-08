8e419aa90d6193293b2043659ba2469ca2d234ed
igorkunovski <igork110@mail.ru>
2022-12-08 11:33:29 +0200

    HW1 Ex4: equation with question marks with Exception handling

--- hw1/Ex4.java
+++ hw1/Ex4.java
@@ -1,3 +1,4 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 /**
@@ -9,33 +10,37 @@ import java.util.Scanner;
 public class Ex4 {
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Insert equation in format 2? + ?5 = 69: ");
-        String eq = sc.nextLine();
-        sc.close();
+        try {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Insert equation in format 2? + ?5 = 69: ");
+            String eq = sc.nextLine();
+            sc.close();
 
-        int result = getResult(eq);
+            int result = getResult(eq);
 
-        String[] expression = get_expr(eq);
-        String left = expression[0];
-        String right = expression[1];
+            String[] expression = get_expr(eq);
+            String left = expression[0];
+            String right = expression[1];
 
-        boolean flag = false;
-        for (int i = 0; i < 10; i++) {
-            int numLeft = Integer.parseInt(left.replace("?", String.valueOf(i)));
+            boolean flag = false;
+            for (int i = 0; i < 10; i++) {
+                int numLeft = Integer.parseInt(left.replace("?", String.valueOf(i)));
 
-            for (int j = 0; j < 10; j++) {
-                int numRight = Integer.parseInt(right.replace("?", String.valueOf(j)));
-                if(result == numLeft + numRight){
-                    System.out.printf("%d + %d = %d", numLeft, numRight, result);
-                    System.out.println();
-                    flag = true;
-                    break;
+                for (int j = 0; j < 10; j++) {
+                    int numRight = Integer.parseInt(right.replace("?", String.valueOf(j)));
+                    if (result == numLeft + numRight) {
+                        System.out.printf("%d + %d = %d", numLeft, numRight, result);
+                        System.out.println();
+                        flag = true;
+                        break;
+                    }
                 }
             }
-        }
-        if (!flag){
-            System.out.println("No solutions!");
+            if (!flag) {
+                System.out.println("No solutions!");
+            }
+        }catch (InputMismatchException | ArrayIndexOutOfBoundsException e){
+            System.out.println("Incorrect data inserted!");
         }
     }
 
